4c794879e30e197fb2e063e997ba17ca49ad79c9
Alejandro Henao <alejandrohenao91@gmail.com>
2023-09-06 08:26:57 -0500

    🎨 Formatting with ruff and black

--- src/model/build.py
+++ src/model/build.py
@@ -1,14 +1,14 @@
+import argparse
+import os
+
 import torch
+import wandb
 
 # Import the model class from the main file
 from src.Classifier import Classifier
 
-import os
-import argparse
-import wandb
-
 parser = argparse.ArgumentParser()
-parser.add_argument('--IdExecution', type=str, help='ID of the execution')
+parser.add_argument("--IdExecution", type=str, help="ID of the execution")
 args = parser.parse_args()
 
 if args.IdExecution:
@@ -25,16 +25,24 @@ if not os.path.exists("./model"):
 num_classes = 10
 input_shape = 784
 
-def build_model_and_log(config, model, model_name="MLP", model_description="Simple MLP"):
-    with wandb.init(project="MLOps-Pycon2023", 
-        name=f"initialize Model ExecId-{args.IdExecution}", 
-        job_type="initialize-model", config=config) as run:
+
+def build_model_and_log(
+    config, model, model_name="MLP", model_description="Simple MLP"
+):
+    with wandb.init(
+        project="MLOps-Pycon2023",
+        name=f"initialize Model ExecId-{args.IdExecution}",
+        job_type="initialize-model",
+        config=config,
+    ) as run:
         config = wandb.config
 
         model_artifact = wandb.Artifact(
-            model_name, type="model",
+            model_name,
+            type="model",
             description=model_description,
-            metadata=dict(config))
+            metadata=dict(config),
+        )
 
         name_artifact_model = f"initialized_model_{model_name}.pth"
 
@@ -49,11 +57,13 @@ def build_model_and_log(config, model, model_name="MLP", model_description="Simp
 
 # MLP
 # Testing config
-model_config = {"input_shape":input_shape,
-                "hidden_layer_1": 32,
-                "hidden_layer_2": 64,
-                "num_classes":num_classes}
+model_config = {
+    "input_shape": input_shape,
+    "hidden_layer_1": 32,
+    "hidden_layer_2": 64,
+    "num_classes": num_classes,
+}
 
 model = Classifier(**model_config)
 
-build_model_and_log(model_config, model, "linear","Simple Linear Classifier")
+build_model_and_log(model_config, model, "linear", "Simple Linear Classifier")
